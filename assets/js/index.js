2a0332304921d01ddfd95e30b7871fb4126d8de6
asteria <0002asteria@proton.me>
2024-11-20 18:10:20 +0100

    breathed color into the project

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ function discord(){
   var discordID = "luna.sh";
   navigator.clipboard.writeText(discordID).then(() =>{
     console.log("You've copied to cliboard!");
+    alert("Copied to clipboard");
   }).catch(e => {
     console.error("Failed to copy text!", e);
   });
