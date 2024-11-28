a44632628e46784888ff116d9bb8faf47a8c3f6c
asteria <0002asteria@proton.me>
2024-11-28 19:34:48 +0100

    trying something out

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,23 @@
-function discord(){
-  var discordID = "luna.sh";
-  navigator.clipboard.writeText(discordID).then(() =>{
-    console.log("You've copied to cliboard!");
-    alert("Copied to clipboard");
-  }).catch(e => {
-    console.error("Failed to copy text!", e);
-  });
+let fetchedData = null;
+let hasFetched = false;
+
+// First, let's check if we've already fetched anything
+
+const fetchSocials = (platform) => {
+  if (hasFetched == false){   
+    fetch ('socials.json')
+    .then(parseSocials => parseSocials.json()) //parsing
+    .then(data => {
+      console.log(`Trying to copy ${data[platform]} into the clipboard;\ncaller: ${platform}.`);
+      return data[platform];
+      fetchedData = data;
+      hasFetched = true;
+    })
+    .catch(err => console.error(`Error:\n${err}\nCaller:${platform}`))
+  }
+  else if (hasFetched == true){
+    return Promise.resolve(fetchedData);
+  }
 }
+
+
